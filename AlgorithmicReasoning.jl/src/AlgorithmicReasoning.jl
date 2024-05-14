9be2c0b6b6bc1e405276944bc7839c917dbd7b68
Ronald Albert <ronaldalbert1609@gmail.com>
2024-05-14 09:33:12 -0300

    :sparkles: Add datamodule

--- AlgorithmicReasoning.jl/src/AlgorithmicReasoning.jl
+++
@@ -1,8 +0,0 @@
-module AlgorithmicReasoning
-
-export CLRS30
-
-include("../CLRS30.jl/src/CLRS30.jl")
-using .CLRS30
-
-end # module AlgorithmicReasoning
--- AlgorithmicReasoning.jl/src/models/Processors.jl
+++
@@ -1,3 +0,0 @@
-module Processors
-    
-end
