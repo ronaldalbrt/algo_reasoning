04e4040753dc4dd8e1a56880a88e665a0afcf632
ronaldalbert <ronaldalbert@ibm.com>
2024-07-29 14:47:16 -0300

    :sparkles: Execute Three Kinds Dice Problem

--- run.py
+++ run.py
@@ -53,7 +53,7 @@ def list_of_strings(arg):
 
 if __name__ == '__main__':
     ap = argparse.ArgumentParser(description='Training Parser Options')
-    ap.add_argument('--algorithms', default=["graham_scan"], type=list_of_strings, help="Algorithms for the model to be trained on.")
+    ap.add_argument('--algorithms', default=["three_kinds_dice"], type=list_of_strings, help="Algorithms for the model to be trained on.")
     ap.add_argument('--path', default="tmp/CLRS30", type=str, help="Path to the dataset folder")
     ap.add_argument('--batch_size', default=8, type=int, help="Number of samples in each training batch")
     ap.add_argument('--n_epochs', default=100, type=int, help="Number of training epochs")
@@ -61,10 +61,10 @@ if __name__ == '__main__':
     ap.add_argument('--lr', default=1e-3, type=float, help="Initial Learning Rate for ADAM Optimizer")
     ap.add_argument('--lr_decrease_factor', default=0.1, type=float, help="Factor by which the learning rate is going to be reduced after lr_patience epochs without Evaluation perfomance improvement.")
     ap.add_argument('--lr_patience', default=10, type=int, help="Number of epochs without improvement for the learning rate to de decreased")
-    ap.add_argument('--model_name', default="GrahamScan_NotPretrained_NoHints", type=str, help="Model's name")
+    ap.add_argument('--model_name', default="ThreeKindsDice_NotPretrained", type=str, help="Model's name")
     ap.add_argument('--checkpoint_path', default="checkpoints/", type=str, help="Path for checkpoints folder")
     ap.add_argument('--checkpoint_model', default="", type=str, help="Path for pretrained checkpoint model")
-    ap.add_argument("--accelerator", default="gpu", type=str, help="Device for the model to be trained on")
+    ap.add_argument("--accelerator", default="cpu", type=str, help="Device for the model to be trained on")
     ap.add_argument("--devices",  default=1, type=str, help="Number of devices used for training")
     ap.add_argument("--processor_pretrained_path", default="", type=str, help="Path for processor's weights folder")
     ap.add_argument("--freeze_processor", default=False, type=bool, help="Whether or not to freeze processor's weights.")
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     processor = None
     if args.processor_pretrained_path != "":
         processor = EncodeProcessDecode(algos).processor
-        state_dict = torch.load(args.processor_pretrained_path)["state_dict"]
+        state_dict = torch.load(args.processor_pretrained_path, map_location=torch.device("cpu"))["state_dict"]
         new_state_dict = {}
         for key in state_dict:
             if "processor" in key:
