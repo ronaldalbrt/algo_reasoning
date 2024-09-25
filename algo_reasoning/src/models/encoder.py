:bug: Appropriate weight initialization + gradient norm tracking

algo_reasoning/src/models/encoder.py
@@ -53,12 +53,15 @@ class Encoder(nn.Module):
             if loc == Location.EDGE and type_ == Type.POINTER:
                 self.encoder[k+"_2"] = LinearEncoder(input_dim, hidden_dim)
 
-        self.init_weights()
+        self.reset_parameters()
 
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_normal_(m.weight)
+    def reset_parameters(self):
+        for k, v in self.encoder.items(): 
+            stage, _, type_ = self.specs[k]
+            m = self.encoder[k]
+
+            if stage == Stage.HINT and type_ == Type.SCALAR:
+                nn.init.xavier_normal_(m.weight) 
 
     def _encode_CLRSData(self, data, node_hidden, edge_hidden, graph_hidden, adj_mat, nb_nodes, hint_step=None):    
         for k, value in data:

algo_reasoning/src/models/network.py
@@ -6,7 +6,7 @@ from .encoder import Encoder
 from .decoder import Decoder
 from .processor import PGN, MPNN
 from algo_reasoning.src.data import CLRSData, CLRSOutput
-from algo_reasoning.src.specs import SPECS, Type, Stage, Location, CATEGORIES_DIMENSIONS
+from algo_reasoning.src.specs import SPECS, CATEGORIES_DIMENSIONS, Type
 
 
 class EncodeProcessDecode(torch.nn.Module):

algo_reasoning/src/models/processor.py
@@ -56,13 +56,9 @@ class PGN(nn.Module):
             self.gate2 = nn.Linear(self.mid_channels, out_channels)
             self.gate3 = nn.Linear(out_channels, out_channels)
 
-        self.init_weights()
+        self.reset_parameters()
 
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Linear):
-                nn.init.xavier_normal_(m.weight, gain=nn.init.calculate_gain('relu'))
-
+    def reset_parameters(self):
         if self.gated:
             nn.init.constant_(self.gate3.weight, -3)
 