b937176260f352ea79d38f58b7ebb093b61aeb14
Ronald Albert <ronaldalbert1609@gmail.com>
2024-10-18 13:05:48 -0300

    :construction: WIP: Sorting algorithms

--- algo_reasoning/src/algorithms/sorting.py
+++ algo_reasoning/src/algorithms/sorting.py
@@ -25,44 +25,49 @@ See "Introduction to Algorithms" 3ed (CLRS3) for more information.
 
 """
 # pylint: disable=invalid-name
-
-
-from typing import Tuple
-
 import torch
 
-from clrs._src import probing
-
 from algo_reasoning.src.data import CLRSData
-from algo_reasoning.src import specs
 
 # TODO: Create probing file
 def probe_array(A_pos):
     """Constructs an `array` probe."""
-    probe = torch.arange(A_pos.shape[0])
-    for i in range(1, A_pos.shape[0]):
+    probe = torch.arange(A_pos.size(0))
+    for i in range(1, A_pos.size(0)):
         probe[A_pos[i]] = A_pos[i - 1]
 
-    return probe.unsqueeze(0).unsqueeze(0)
+    return probe
 
 def mask_one(i, n):
     """Constructs a `mask_one` probe."""
     assert n > i
     probe = torch.zeros(n)
     probe[i] = 1
-    return probe.unsqueeze(0).unsqueeze(0)
+    return probe
+
+
+def heap(A_pos, heap_size):
+    """Constructs a `heap` probe."""
+    assert heap_size > 0
+    
+    probe = torch.arange(A_pos.size(0))
+    
+    for i in range(1, heap_size):
+        probe[A_pos[i]] = A_pos[(i - 1) // 2]
+    
+    return probe
 
 def insertion_sort(A, nb_nodes, *args, **kwargs):
     """Insertion sort."""
     data = CLRSData(algorithm="insertion_sort", *args, **kwargs)
     
-    data.set_inputs({
-        'A': A.float().unsqueeze(0)
+    data = data.set_inputs({
+        'key': A
     }, nb_nodes)
 
     A_pos = torch.arange(A.size(0))
 
-    data.increase_hints({
+    data = data.increase_hints({
         'pred_h': probe_array(A_pos.clone()),
         'i': mask_one(0, A.size(0)),
         'j': mask_one(0, A.size(0))
@@ -80,13 +85,13 @@ def insertion_sort(A, nb_nodes, *args, **kwargs):
         stor_pos = A_pos[i + 1].item()
         A_pos[i + 1] = j
 
-        data.increase_hints({
+        data = data.increase_hints({
             'pred_h': probe_array(A_pos.clone()),
             'i': mask_one(stor_pos, A.size(0)),
             'j': mask_one(j, A.size(0))
         })
     
-    data.set_outputs({
+    data = data.set_outputs({
         'pred': probe_array(A_pos.clone()),
     })
 
@@ -97,20 +102,20 @@ def bubble_sort(A, nb_nodes, *args, **kwargs):
     """Bubble sort."""
     data = CLRSData(algorithm="insertion_sort", *args, **kwargs)
     
-    data.set_inputs({
-        'A': A.float().unsqueeze(0)
+    data = data.set_inputs({
+        'key': A
     }, nb_nodes)
 
     A_pos = torch.arange(A.size(0))
 
-    data.increase_hints({
+    data = data.increase_hints({
         'pred_h': probe_array(A_pos.clone()),
         'i': mask_one(0, A.size(0)),
         'j': mask_one(0, A.size(0))
     })
 
-    for i in range(A.shape[0] - 1):
-        for j in reversed(range(i + 1, A.shape[0])):
+    for i in range(A.size(0) - 1):
+        for j in reversed(range(i + 1, A.size(0))):
             if A[j] < A[j - 1]:
                 tmp = A[j].item()
                 A[j] = A[j - 1].item()
@@ -120,201 +125,153 @@ def bubble_sort(A, nb_nodes, *args, **kwargs):
                 A_pos[j] = A_pos[j - 1].item()
                 A_pos[j - 1] = tmp
 
-                data.increase_hints({
+                data = data.increase_hints({
                     'pred_h': probe_array(A_pos.clone()),
                     'i': mask_one(A_pos[i], A.size(0)),
                     'j': mask_one(A_pos[j], A.size(0))
                 })
 
-    data.set_outputs({
+    data = data.set_outputs({
        'pred': probe_array(A_pos.clone())
     })
 
     return data
 
 
-# def heapsort(A: _Array) -> _Out:
-#   """Heapsort (Williams, 1964)."""
+def heapsort(A, nb_nodes, *args, **kwargs):
+    """Heapsort (Williams, 1964)."""
+    data = CLRSData(algorithm="heapsort", *args, **kwargs)
+    
+    data = data.set_inputs({
+        'key': A
+    }, nb_nodes)
 
-#   chex.assert_rank(A, 1)
-#   probes = probing.initialize(specs.SPECS['heapsort'])
+    A_pos = torch.arange(A.size(0))
 
-#   A_pos = np.arange(A.shape[0])
-
-#   probing.push(
-#       probes,
-#       specs.Stage.INPUT,
-#       next_probe={
-#           'pos': np.copy(A_pos) * 1.0 / A.shape[0],
-#           'key': np.copy(A)
-#       })
-
-#   probing.push(
-#       probes,
-#       specs.Stage.HINT,
-#       next_probe={
-#           'pred_h': probing.array(np.copy(A_pos)),
-#           'parent': probing.heap(np.copy(A_pos), A.shape[0]),
-#           'i': probing.mask_one(A.shape[0] - 1, A.shape[0]),
-#           'j': probing.mask_one(A.shape[0] - 1, A.shape[0]),
-#           'largest': probing.mask_one(A.shape[0] - 1, A.shape[0]),
-#           'heap_size': probing.mask_one(A.shape[0] - 1, A.shape[0]),
-#           'phase': probing.mask_one(0, 3)
-#       })
-
-#   def max_heapify(A, i, heap_size, ind, phase):
-#     l = 2 * i + 1
-#     r = 2 * i + 2
-#     if l < heap_size and A[l] > A[i]:
-#       largest = l
-#     else:
-#       largest = i
-#     if r < heap_size and A[r] > A[largest]:
-#       largest = r
-#     if largest != i:
-#       tmp = A[i]
-#       A[i] = A[largest]
-#       A[largest] = tmp
-
-#       tmp = A_pos[i]
-#       A_pos[i] = A_pos[largest]
-#       A_pos[largest] = tmp
-
-#     probing.push(
-#         probes,
-#         specs.Stage.HINT,
-#         next_probe={
-#             'pred_h': probing.array(np.copy(A_pos)),
-#             'parent': probing.heap(np.copy(A_pos), heap_size),
-#             'i': probing.mask_one(A_pos[ind], A.shape[0]),
-#             'j': probing.mask_one(A_pos[i], A.shape[0]),
-#             'largest': probing.mask_one(A_pos[largest], A.shape[0]),
-#             'heap_size': probing.mask_one(A_pos[heap_size - 1], A.shape[0]),
-#             'phase': probing.mask_one(phase, 3)
-#         })
-
-#     if largest != i:
-#       max_heapify(A, largest, heap_size, ind, phase)
-
-#   def build_max_heap(A):
-#     for i in reversed(range(A.shape[0])):
-#       max_heapify(A, i, A.shape[0], i, 0)
-
-#   build_max_heap(A)
-#   heap_size = A.shape[0]
-#   for i in reversed(range(1, A.shape[0])):
-#     tmp = A[0]
-#     A[0] = A[i]
-#     A[i] = tmp
-
-#     tmp = A_pos[0]
-#     A_pos[0] = A_pos[i]
-#     A_pos[i] = tmp
-
-#     heap_size -= 1
-
-#     probing.push(
-#         probes,
-#         specs.Stage.HINT,
-#         next_probe={
-#             'pred_h': probing.array(np.copy(A_pos)),
-#             'parent': probing.heap(np.copy(A_pos), heap_size),
-#             'i': probing.mask_one(A_pos[0], A.shape[0]),
-#             'j': probing.mask_one(A_pos[i], A.shape[0]),
-#             'largest': probing.mask_one(0, A.shape[0]),  # Consider masking
-#             'heap_size': probing.mask_one(A_pos[heap_size - 1], A.shape[0]),
-#             'phase': probing.mask_one(1, 3)
-#         })
-
-#     max_heapify(A, 0, heap_size, i, 2)  # reduce heap_size!
-
-#   probing.push(
-#       probes,
-#       specs.Stage.OUTPUT,
-#       next_probe={'pred': probing.array(np.copy(A_pos))},
-#   )
-
-#   probing.finalize(probes)
-
-#   return A, probes
+    data = data.increase_hints({
+          'pred_h': probe_array(A_pos.clone()),
+          'parent': heap(A_pos.clone(), A.size(0)),
+          'i': mask_one(A.size(0) - 1, A.size(0)),
+          'j': mask_one(A.size(0) - 1, A.size(0)),
+          'largest': mask_one(A.size(0) - 1, A.size(0)),
+          'heap_size': mask_one(A.size(0) - 1, A.size(0)),
+          'phase': mask_one(0, 3)
+    })
 
 
-# def quicksort(A: _Array, A_pos=None, p=None, r=None, probes=None) -> _Out:
-#   """Quicksort (Hoare, 1962)."""
+    def max_heapify(A, i, heap_size, ind, phase, data):
+        l = 2 * i + 1
+        r = 2 * i + 2
+        if l < heap_size and A[l] > A[i]:
+            largest = l
+        else:
+            largest = i
+        if r < heap_size and A[r] > A[largest]:
+            largest = r
+        if largest != i:
+            A[i], A[largest] = A[largest].item(), A[i].item()
+            A_pos[i], A_pos[largest] = A_pos[largest].item(), A_pos[i].item()
 
-#   chex.assert_rank(A, 1)
+        data = data.increase_hints({
+          'pred_h': probe_array(A_pos.clone()),
+          'parent': heap(A_pos.clone(), heap_size),
+          'i': mask_one(A_pos[ind].item(), A.size(0)),
+          'j': mask_one(A_pos[i].item(), A.size(0)),
+          'largest': mask_one(A_pos[largest].item(), A.size(0)),
+          'heap_size': mask_one(A_pos[heap_size - 1].item(), A.size(0)),
+          'phase': mask_one(phase, 3)
+        })
 
-#   def partition(A, A_pos, p, r, probes):
-#     x = A[r]
-#     i = p - 1
-#     for j in range(p, r):
-#       if A[j] <= x:
-#         i += 1
-#         tmp = A[i]
-#         A[i] = A[j]
-#         A[j] = tmp
-#         tmp = A_pos[i]
-#         A_pos[i] = A_pos[j]
-#         A_pos[j] = tmp
+        if largest != i:
+            max_heapify(A, largest, heap_size, ind, phase, data)
 
-#       probing.push(
-#           probes,
-#           specs.Stage.HINT,
-#           next_probe={
-#               'pred_h': probing.array(np.copy(A_pos)),
-#               'p': probing.mask_one(A_pos[p], A.shape[0]),
-#               'r': probing.mask_one(A_pos[r], A.shape[0]),
-#               'i': probing.mask_one(A_pos[i + 1], A.shape[0]),
-#               'j': probing.mask_one(A_pos[j], A.shape[0])
-#           })
+    def build_max_heap(A):
+        for i in reversed(range(A.size(0))):
+            max_heapify(A, i, A.size(0), i, 0, data)
 
-#     tmp = A[i + 1]
-#     A[i + 1] = A[r]
-#     A[r] = tmp
-#     tmp = A_pos[i + 1]
-#     A_pos[i + 1] = A_pos[r]
-#     A_pos[r] = tmp
+    build_max_heap(A)
+    heap_size = A.size(0)
 
-#     probing.push(
-#         probes,
-#         specs.Stage.HINT,
-#         next_probe={
-#             'pred_h': probing.array(np.copy(A_pos)),
-#             'p': probing.mask_one(A_pos[p], A.shape[0]),
-#             'r': probing.mask_one(A_pos[r], A.shape[0]),
-#             'i': probing.mask_one(A_pos[i + 1], A.shape[0]),
-#             'j': probing.mask_one(A_pos[r], A.shape[0])
-#         })
+    for i in reversed(range(1, A.size(0))):
+        A[0], A[i] = A[i].item(), A[0].item()
+        A_pos[0], A_pos[i] = A_pos[i].item(), A_pos[0].item()
 
-#     return i + 1
+        heap_size -= 1
 
-#   if A_pos is None:
-#     A_pos = np.arange(A.shape[0])
-#   if p is None:
-#     p = 0
-#   if r is None:
-#     r = len(A) - 1
-#   if probes is None:
-#     probes = probing.initialize(specs.SPECS['quicksort'])
-#     probing.push(
-#         probes,
-#         specs.Stage.INPUT,
-#         next_probe={
-#             'pos': np.copy(A_pos) * 1.0 / A.shape[0],
-#             'key': np.copy(A)
-#         })
+        data = data.increase_hints({
+          'pred_h': probe_array(A_pos.clone()),
+          'parent': heap(A_pos.clone(), heap_size),
+          'i': mask_one(A_pos[0].item(), A.size(0)),
+          'j': mask_one(A_pos[i].item(), A.size(0)),
+          'largest': mask_one(0, A.size(0)),
+          'heap_size': mask_one(A_pos[heap_size - 1].item(), A.size(0)),
+          'phase': mask_one(1, 3)
+        })
 
-#   if p < r:
-#     q = partition(A, A_pos, p, r, probes)
-#     quicksort(A, A_pos, p, q - 1, probes)
-#     quicksort(A, A_pos, q + 1, r, probes)
 
-#   if p == 0 and r == len(A) - 1:
-#     probing.push(
-#         probes,
-#         specs.Stage.OUTPUT,
-#         next_probe={'pred': probing.array(np.copy(A_pos))},
-#     )
-#     probing.finalize(probes)
+        max_heapify(A, 0, heap_size, i, 2, data) 
 
-#   return A, probes
+    data = data.set_outputs({
+       'pred': probe_array(A_pos.clone())
+    })
+
+    return data
+
+def quicksort(A, nb_nodes, A_pos=None, p=None, r=None, data=None, *args, **kwargs):
+    """Quicksort (Hoare, 1962)."""
+
+    def partition(A, A_pos, p, r, data):
+        print("Partitioning", data)
+        x = A[r]
+        i = p - 1
+        for j in range(p, r):
+            if A[j] <= x:
+                i += 1
+                A[i], A[j] = A[j], A[i]
+                A_pos[i], A_pos[j] = A_pos[j], A_pos[i]
+            
+            data = data.increase_hints({
+                'pred_h': probe_array(A_pos.clone()),
+                'p': mask_one(A_pos[p].item(), A.size(0)),
+                'r': mask_one(A_pos[r].item(), A.size(0)),
+                'i': mask_one(A_pos[i + 1].item(), A.size(0)),
+                'j': mask_one(A_pos[j].item(), A.size(0))
+            })
+
+        A[i + 1], A[r] = A[r].item(), A[i + 1].item()
+        A_pos[i + 1], A_pos[r] = A_pos[r].item(), A_pos[i + 1].item()
+
+        data = data.increase_hints({
+            'pred_h': probe_array(A_pos.clone()),
+            'p': mask_one(A_pos[p].item(), A.size(0)),
+            'r': mask_one(A_pos[r].item(), A.size(0)),
+            'i': mask_one(A_pos[i + 1].item(), A.size(0)),
+            'j': mask_one(A_pos[r].item(), A.size(0))
+        })
+
+        return i + 1
+
+    if A_pos is None:
+        A_pos = torch.arange(A.size(0))
+    if p is None:
+        p = 0
+    if r is None:
+        r = A.size(0) - 1
+    if data is None:
+        data = CLRSData(algorithm="quicksort", *args, **kwargs)
+
+        data = data.set_inputs({
+            'key': A
+        }, nb_nodes)
+
+    if p < r:
+        q = partition(A, A_pos, p, r, data)
+        quicksort(A, nb_nodes, A_pos, p, q - 1, data, *args, **kwargs)
+        quicksort(A, nb_nodes, A_pos, q + 1, r, data, *args, **kwargs)
+
+    if p == 0 and r == len(A) - 1:
+        data = data.set_outputs({
+            'pred': probe_array(A_pos.clone())
+        })
+        
+    return data
