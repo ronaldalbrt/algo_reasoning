10a4483882b58a3ed8de39f66b7567bf9bffd273
ronaldalbrt <ronaldalbert1609@gmail.com>
2024-10-27 17:57:30 -0300

    :bug: Fix CLI run.py

--- run.py
+++ run.py
@@ -33,11 +33,11 @@ ap.add_argument('--batch_size', default=32, type=int,
                 help="Number of samples in each training batch")
 ap.add_argument('--n_epochs', default=100, type=int,
                 help="Number of training epochs")
-ap.add_argument('train_steps', default=100, type=int,
+ap.add_argument('--train_steps', default=100, type=int,
                 help='Number of training steps per epoch and algorithm')
-ap.add_argument('val_steps', default=32, type=int,
+ap.add_argument('--val_steps', default=32, type=int,
                 help="Number of validation steps per algorithm")
-ap.add_argument('test_steps', default=32, type=int,
+ap.add_argument('--test_steps', default=32, type=int,
                 help="Number of test steps per algorithm")
 ap.add_argument('--n_workers', default=8, type=int,
                 help="Number of Data Loading Workers")
