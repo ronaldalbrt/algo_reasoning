1e3f64478e76f232cb583255f07bd674f0d7325f
ronaldalbrt <ronaldalbert1609@gmail.com>
2024-11-25 12:02:09 -0300

    :construction: WIP: test for batched data

--- algo_reasoning/src/utils.py
+++ algo_reasoning/src/utils.py
@@ -20,8 +20,8 @@ def normal_log_pdf(sample, mu, sigma2):
     return -0.5*(((sample - mu)**2)/(sigma2) + normalization_term)
 
 def multivariatenormal_log_pdf(sample, mu, cov):
-    n_dim = sample.size(1)
-    normalization_term = n_dim*torch.log(2*torch.pi)
+    n_dim = sample.size(0)
+    normalization_term = n_dim*math.log(2*torch.pi)
     inv_cov = torch.linalg.inv(cov)
 
-    return -0.5*((sample - mu).T@inv_cov@(sample-mu) + torch.det(cov) + normalization_term)
+    return -0.5*((sample - mu).T@inv_cov@(sample-mu) + torch.log(torch.det(cov)) + normalization_term)
