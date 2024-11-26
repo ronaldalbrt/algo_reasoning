:bug: Fix to logprob

algo_reasoning/src/utils.py
@@ -21,7 +21,11 @@ def normal_log_pdf(sample, mu, sigma2):
 
 def multivariatenormal_log_pdf(sample, mu, cov):
     n_dim = sample.size(1)
-    normalization_term = n_dim*math.log(2*torch.pi)
-    inv_cov = torch.linalg.inv(cov)
 
-    return -0.5*((((sample - mu)@inv_cov) * (sample - mu)).sum(dim=1) + torch.log(torch.det(cov)) + normalization_term)
+    det_log = torch.logdet(cov) 
+    det_log = det_log if not (torch.isnan(det_log).item() or det_log.item() == float('-inf')) else 0
+
+    normalization_term = n_dim*math.log(2*torch.pi) + det_log
+    inv_cov = torch.linalg.pinv(cov)
+
+    return -0.5*((((sample - mu)@inv_cov) * (sample - mu)).sum(dim=1) + normalization_term)