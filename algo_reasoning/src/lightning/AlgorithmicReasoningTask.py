c6731ec02a1c7805ea4eefa9560c08b20d9801f8
Ronald Albert <ronaldalbert1609@gmail.com>
2025-01-03 09:38:22 -0300

    :fire: Remove unused regularizations terms from loss

--- algo_reasoning/src/lightning/AlgorithmicReasoningTask.py
+++ algo_reasoning/src/lightning/AlgorithmicReasoningTask.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2024 Ronald Albert ronaldalbert@cos.ufrj.br
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#         http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch.nn.functional as F
 import lightning as L
 from algo_reasoning.src.eval import eval_function
@@ -21,7 +35,7 @@ class AlgorithmicReasoningTask(L.LightningModule):
         preds = output["output"]
         hidden = output["hidden_embeddings"]
 
-        loss = self.loss_fn(preds, batch, hidden, self.current_epoch)
+        loss = self.loss_fn(preds, batch, hidden)
 
         if calculate_metrics:
             metrics_r = eval_function(preds.detach().cpu(), batch.detach().cpu())
--- algo_reasoning/src/losses/AlgorithmicReasoningLoss.py
+++ algo_reasoning/src/losses/AlgorithmicReasoningLoss.py
@@ -1,3 +1,17 @@
+# Copyright (C) 2024 Ronald Albert ronaldalbert@cos.ufrj.br
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#         http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -5,44 +19,19 @@ import math
 from torch import autograd
 
 from algo_reasoning.src.specs import SPECS, Type, OutputClass
-from algo_reasoning.src.utils import multivariatenormal_log_pdf, normal_log_pdf
-
-REGULARIZATION_TYPES = ["constant_eigen",  "independence_term", "irm_penalty"]
     
 class AlgorithmicReasoningLoss(nn.Module):
-    def __init__(self, hint_loss_weight=0.1, reg_weight=0.0, reg_type="constant_eigen"):
+    def __init__(self, hint_loss_weight=0.1, reg_weight=0.0):
         super().__init__()
         self.hint_loss = (hint_loss_weight > 0.0)
         self.hint_loss_weight = hint_loss_weight
         self.reg_weight = reg_weight
-        self.reg_type = reg_type
-        self.reg_term = self.reg_weight > 0.0 and reg_type in REGULARIZATION_TYPES
+        self.reg_term = self.reg_weight > 0.0
 
         self.dummy_w =  nn.Parameter(torch.tensor(1.))
         
         if self.reg_term:
-            if reg_type == "constant_eigen":
-                self.regularizer = lambda emb: torch.mean(torch.abs(torch.sum(emb, dim=2)/(torch.norm(emb, dim=2)*(math.sqrt(emb.size(2))))))
-            elif reg_type == "independence_term":
-                def kl_div(emb):
-                    emb = emb.flatten(0, 2)
-
-                    mu = emb.mean(dim=0)
-                    sigma = torch.cov(emb.T)
-
-                    log_q = multivariatenormal_log_pdf(emb, mu, sigma)
-                    log_qi = normal_log_pdf(emb, mu,  torch.diagonal(sigma, 0))
-
-                    return torch.exp((-log_q + log_qi.sum(dim=1)).mean())
-                
-                self.regularizer = kl_div
-            elif reg_type == "irm_penalty":
-                def irm_penalty(losses):
-                    grad = autograd.grad(losses.mean(), [self.dummy_w,], create_graph=True)[0]
-
-                    return torch.sum(grad**2)
-                
-                self.regularizer = irm_penalty
+            self.regularizer = lambda emb: torch.mean(torch.abs(torch.sum(emb, dim=2)/(torch.norm(emb, dim=2)*(math.sqrt(emb.size(2))))))
 
     def _calculate_loss(self, mask, truth, pred, type_, nb_nodes):
         dim_to_reduce = list(range(1, pred.dim()))
@@ -91,9 +80,9 @@ class AlgorithmicReasoningLoss(nn.Module):
             mask = torch.ones_like(batch.outputs[key], device=device)
 
             if output_loss is None:
-                output_loss = self._calculate_loss(mask, batch.outputs[key], self.dummy_w * value, type_, nb_nodes)
+                output_loss = self._calculate_loss(mask, batch.outputs[key], value, type_, nb_nodes)
             else:
-                output_loss += self._calculate_loss(mask, batch.outputs[key], self.dummy_w * value,  type_, nb_nodes)
+                output_loss += self._calculate_loss(mask, batch.outputs[key], value,  type_, nb_nodes)
 
 
         if self.hint_loss:
@@ -110,15 +99,15 @@ class AlgorithmicReasoningLoss(nn.Module):
                 ground_truth = batch.hints[key][:, :max_length]
 
                 if hint_loss is None:
-                    hint_loss = self._calculate_loss(mask, ground_truth, self.dummy_w * value, type_, nb_nodes)
+                    hint_loss = self._calculate_loss(mask, ground_truth, value, type_, nb_nodes)
                 else:
-                    hint_loss += self._calculate_loss(mask, ground_truth, self.dummy_w * value, type_, nb_nodes)
+                    hint_loss += self._calculate_loss(mask, ground_truth, value, type_, nb_nodes)
 
             output_loss += self.hint_loss_weight*hint_loss if hint_loss is not None else 0
 
         reg_weight = self.reg_weight
         if self.reg_term and self.training:
-            reg_loss = self.regularizer(output_loss) if self.reg_type == "irm_penalty" else self.regularizer(hidden)
+            reg_loss = self.regularizer(hidden)
             
         else:
             reg_loss = 0.0
