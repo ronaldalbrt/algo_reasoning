02589bd8a3fa2924e1b62b394fbc9b9fb6447171
Ronald Albert <ronaldalbert1609@gmail.com>
2025-01-20 10:44:13 -0300

    :sparkles: Update error_run

--- experiments/error_run.sh
+++ experiments/error_run.sh
@@ -1,16 +1,32 @@
-python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name mpnn --processor_model mpnn --batch_size 16
-python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name spectralmpnn --processor_model spectralmpnn --batch_size 16
-python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name specformer --processor_model specformer --batch_size 16
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name mpnn0 --processor_model mpnn --batch_size 16
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name mpnn1 --processor_model mpnn --batch_size 16
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name mpnn2 --processor_model mpnn --batch_size 16
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name mpnn3 --processor_model mpnn --batch_size 16
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name mpnn4 --processor_model mpnn --batch_size 16
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name spectralmpnn0 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name spectralmpnn1 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name spectralmpnn2 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name spectralmpnn3 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name spectralmpnn4 --processor_model spectralmpnn2 --batch_size 16
 
-python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name mpnn --processor_model mpnn --batch_size 16
-python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name spectralmpnn --processor_model spectralmpnn --batch_size 16
-python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name specformer --processor_model specformer --batch_size 16
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name mpnn0 --processor_model mpnn --batch_size 16
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name mpnn1 --processor_model mpnn --batch_size 16
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name mpnn2 --processor_model mpnn --batch_size 16
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name mpnn3 --processor_model mpnn --batch_size 16
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name mpnn4 --processor_model mpnn --batch_size 16
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name spectralmpnn0 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name spectralmpnn1 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name spectralmpnn2 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name spectralmpnn3 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name spectralmpnn4 --processor_model spectralmpnn2 --batch_size 16
 
-python run.py --algorithms optimal_bst --model_name optimal_bst --version_name mpnn --processor_model mpnn --batch_size 16
-python run.py --algorithms optimal_bst --model_name optimal_bst --version_name spectralmpnn --processor_model spectralmpnn --batch_size 16
-python run.py --algorithms optimal_bst --model_name optimal_bst --version_name specformer --processor_model specformer --batch_size 16
-
-python run.py --algorithms graham_scan --model_name graham_scan --version_name spectralmpnn --processor_model spectralmpnn
-
-python run.py --algorithms jarvis_march --model_name jarvis_march --version_name spectralmpnn --processor_model spectralmpnn
-python run.py --algorithms jarvis_march --model_name jarvis_march --version_name specformer --processor_model specformer
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name mpnn0 --processor_model mpnn --batch_size 16
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name mpnn1 --processor_model mpnn --batch_size 16
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name mpnn2 --processor_model mpnn --batch_size 16
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name mpnn3 --processor_model mpnn --batch_size 16
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name mpnn4 --processor_model mpnn --batch_size 16
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name spectralmpnn0 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name spectralmpnn1 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name spectralmpnn2 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name spectralmpnn3 --processor_model spectralmpnn2 --batch_size 16
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name spectralmpnn4 --processor_model spectralmpnn2 --batch_size 16
