:bug: Fix ChebyNet normalization

algo_reasoning/src/models/processor.py
@@ -621,8 +621,8 @@ class ChebyshevGraphConv(nn.Module):
 
         # ChebNet normalization of the laplacian matrix
         lap = normalized_laplacian(adj_matrix)
-        eigval_max = torch.norm(lap, p=2)
-        cheb_lap = 2 * cheb_lap / eigval_max - torch.eye(adj_matrix.size(1), device=adj_matrix.device) 
+        eigval_max = torch.linalg_matrix.norm(lap, ord=2)
+        cheb_lap = (2 * lap / eigval_max) - torch.eye(adj_matrix.size(1), device=adj_matrix.device) 
 
         h = self.feat_encoder(z)
 