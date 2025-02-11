3d8b35b8d91defd434a1598bf6c337c44021292e
Ronald Albert <ronaldalbert1609@gmail.com>
2025-02-11 15:19:49 -0300

    :bug: Fix memory in experiments

--- experiments/run_spectralmpnn_exclusively.sh
+++ experiments/run_spectralmpnn_exclusively.sh
@@ -20,7 +20,7 @@ python run.py --algorithms dijkstra --model_name dijkstra --version_name spectra
 
 python run.py --algorithms find_maximum_subarray_kadane --model_name find_maximum_subarray_kadane --version_name spectralmpnn --processor_model spectralmpnn
 
-python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name spectralmpnn --processor_model spectralmpnn
+python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name spectralmpnn --processor_model spectralmpnn --batch_size 16
 
 python run.py --algorithms graham_scan --model_name graham_scan --version_name spectralmpnn --processor_model spectralmpnn
 
@@ -34,38 +34,26 @@ python run.py --algorithms kmp_matcher --model_name kmp_matcher --version_name s
 
 python run.py --algorithms lcs_length --model_name lcs_length --version_name spectralmpnn --processor_model spectralmpnn
 
-
-python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name spectralmpnn --processor_model spectralmpnn
-
+python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name spectralmpnn --processor_model spectralmpnn --batch_size 16
 
 python run.py --algorithms minimum --model_name minimum --version_name spectralmpnn --processor_model spectralmpnn
 
-
 python run.py --algorithms mst_kruskal --model_name mst_kruskal --version_name spectralmpnn --processor_model spectralmpnn
-
 
 python run.py --algorithms mst_prim --model_name mst_prim --version_name spectralmpnn --processor_model spectralmpnn
 
-
 python run.py --algorithms naive_string_matcher --model_name naive_string_matcher --version_name spectralmpnn --processor_model spectralmpnn
 
-
-python run.py --algorithms optimal_bst --model_name optimal_bst --version_name spectralmpnn --processor_model spectralmpnn
-
+python run.py --algorithms optimal_bst --model_name optimal_bst --version_name spectralmpnn --processor_model spectralmpnn --batch_size 16
 
 python run.py --algorithms quickselect --model_name quickselect --version_name spectralmpnn --processor_model spectralmpnn
 
-
 python run.py --algorithms quicksort --model_name quicksort --version_name spectralmpnn --processor_model spectralmpnn
-
 
 python run.py --algorithms segments_intersect --model_name segments_intersect --version_name spectralmpnn --processor_model spectralmpnn
 
-
 python run.py --algorithms strongly_connected_components --model_name strongly_connected_components --version_name spectralmpnn --processor_model spectralmpnn
 
-
 python run.py --algorithms task_scheduling --model_name task_scheduling --version_name spectralmpnn --processor_model spectralmpnn
-
 
 python run.py --algorithms topological_sort --model_name topological_sort --version_name spectralmpnn --processor_model spectralmpnn
