a3df17fcf2d2ab297aa761d921e5a5d9713c2ebe
Ronald Albert <ronaldalbert1609@gmail.com>
2025-03-17 09:39:30 -0300

    :art: Remove unnecessary scripts for experiments

--- experiments/run_chebnet_experiment.sh
+++ experiments/run_chebnet_experiment.sh
@@ -1,59 +1,38 @@
-python run.py --algorithms articulation_points --model_name articulation_points --version_name chebconv --processor_model chebconv
+run_algorithm () {
+    python run.py --algorithms $1 --model_name $1 --version_name chebconv0 --processor_model chebconv --batch_size $2
+    python run.py --algorithms $1 --model_name $1 --version_name chebconv1 --processor_model chebconv --batch_size $2
+    python run.py --algorithms $1 --model_name $1 --version_name chebconv2 --processor_model chebconv --batch_size $2
+    python run.py --algorithms $1 --model_name $1 --version_name chebconv3 --processor_model chebconv --batch_size $2
+    python run.py --algorithms $1 --model_name $1 --version_name chebconv4 --processor_model chebconv --batch_size $2
+}
 
-python run.py --algorithms activity_selector --model_name activity_selector --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms bellman_ford --model_name bellman_ford --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms bfs --model_name bfs --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms binary_search --model_name binary_search --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms bridges --model_name bridges --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms bubble_sort --model_name bubble_sort --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms dag_shortest_paths --model_name dag_shortest_paths --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms dfs --model_name dfs --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms dijkstra --model_name dijkstra --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms find_maximum_subarray_kadane --model_name find_maximum_subarray_kadane --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms floyd_warshall --model_name floyd_warshall --version_name chebconv --processor_model chebconv --batch_size 16
-
-python run.py --algorithms graham_scan --model_name graham_scan --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms heapsort --model_name heapsort --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms insertion_sort --model_name insertion_sort --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms jarvis_march --model_name jarvis_march --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms kmp_matcher --model_name kmp_matcher --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms lcs_length --model_name lcs_length --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms matrix_chain_order --model_name matrix_chain_order --version_name chebconv --processor_model chebconv --batch_size 16
-
-python run.py --algorithms minimum --model_name minimum --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms mst_kruskal --model_name mst_kruskal --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms mst_prim --model_name mst_prim --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms naive_string_matcher --model_name naive_string_matcher --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms optimal_bst --model_name optimal_bst --version_name chebconv --processor_model chebconv --batch_size 16
-
-python run.py --algorithms quickselect --model_name quickselect --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms quicksort --model_name quicksort --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms segments_intersect --model_name segments_intersect --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms strongly_connected_components --model_name strongly_connected_components --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms task_scheduling --model_name task_scheduling --version_name chebconv --processor_model chebconv
-
-python run.py --algorithms topological_sort --model_name topological_sort --version_name chebconv --processor_model chebconv
+run_algorithm articulation_points 32
+run_algorithm activity_selector 32
+run_algorithm bellman_ford 32
+run_algorithm bfs 32
+run_algorithm binary_search 32
+run_algorithm bridges 32
+run_algorithm bubble_sort 32
+run_algorithm dag_shortest_paths 32
+run_algorithm dfs 32
+run_algorithm dijkstra 32
+run_algorithm find_maximum_subarray_kadane 32
+run_algorithm floyd_warshall 16
+run_algorithm graham_scan 32
+run_algorithm heapsort 32
+run_algorithm insertion_sort 32
+run_algorithm jarvis_march 16
+run_algorithm kmp_matcher 32
+run_algorithm lcs_length 32
+run_algorithm matrix_chain_order 16
+run_algorithm minimum 32
+run_algorithm mst_kruskal 32
+run_algorithm mst_prim 32
+run_algorithm naive_string_matcher 32
+run_algorithm optimal_bst 16
+run_algorithm quickselect 32
+run_algorithm quicksort 32
+run_algorithm segments_intersect 32
+run_algorithm strongly_connected_components 32
+run_algorithm task_scheduling 32
+run_algorithm topological_sort 32
